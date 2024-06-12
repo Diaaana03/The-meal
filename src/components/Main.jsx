cd0c6493ec377d896df9972b591de1599126cbdc
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-06-12 22:54:27 +0600

    added random meal url

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import classes from "./Main.module.css";
 
-function Main() {
+const randomURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+export const Main = () => {
+  const [meal, setMeal] = useState(null);
+
+  useEffect(() => {
+    const randomMeal = async () => {
+      try {
+        const response = await axios.get(randomURL);
+        setMeal(response.data.meals[0]);
+        console.log(response.data);
+      } catch (error) {
+        console.log("Error fetching random meal:", error);
+      }
+    };
+    randomMeal();
+  }, []);
   return (
     <>
       <div className={classes.main}>
@@ -10,11 +27,15 @@ function Main() {
           <div className={classes.random__meal_text}>
             <h2 className={classes.random__meal_h2}>Meal of the Day</h2>
             <p className={classes.random__meal_title}>
-              <Link to="#">title</Link>
+              <Link to={`/meal/${meal.idMeal}`}>{meal.strMeal}</Link>
             </p>
-            <p className={classes.random__meal_info}>type</p>
+            <p className={classes.random__meal_info}>
+              {meal.strCategory} | {meal.strArea}
+            </p>
           </div>
-          <div className={classes.random__meal_img}></div>
+          <div className={classes.random__meal_img}>
+            <img src={meal.strMealThumb} alt={meal.strMeal} />
+          </div>
         </section>
         <section className={classes.form}>
           <h2 className={classes.search__h2}>Find your Meal</h2>
@@ -32,6 +53,4 @@ function Main() {
       </div>
     </>
   );
-}
-
-export default Main;
+};
